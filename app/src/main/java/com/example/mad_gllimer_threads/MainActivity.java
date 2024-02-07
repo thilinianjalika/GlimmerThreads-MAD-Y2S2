f6c6822c24eb20b8e4544ce063f3d143ee82da1a
thilinianjalika <brittothilini@gmail.com>
2024-02-07 21:10:40 +0530

    Gimmer threads mobile application code development

---
+++ app/src/main/java/com/example/mad_gllimer_threads/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.mad_gllimer_threads;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        System.out.println("Pehesara");
+        System.out.println("Pehesara");
+    }
+}
